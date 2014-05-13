e75c521cf1b4fe34285c46f1bcc2b759c46128b4
ravirsagar <ravi.sagar@washpost.com>
2014-05-13 15:48:36 -0400

    Git Test11

---
+++ Dumm.h
@@ -0,0 +1,15 @@
+//
+//  DummyAppDelegate.h
+//  DummyProj1
+//
+//  Created by Sagar, Ravi on 5/8/14.
+//  Copyright (c) 2014 Wapo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
